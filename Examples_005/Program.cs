e38b3c2ba04b215b1c385b68ac7ecd61fae3cfcc
Bad_S <Evlampyev.Alex@gmail.com>
2022-10-11 22:44:33 +0500

    Exam_05

---
+++ Examples_005/Program.cs
@@ -0,0 +1,6 @@
+﻿int nameA = new Random().Next(1,10);
+Console.WriteLine(nameA);
+int nameB = new Random().Next(1,10);
+Console.WriteLine(nameB);
+int result = nameA+ nameB;
+Console.WriteLine(result);
