3a915f10c308ac7e64d38e07776c295c27b9725c
Bad_S <Evlampyev.Alex@gmail.com>
2022-10-11 22:20:50 +0500

    Exam_003

---
+++ Examples_003/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите первое число ");
+string namberA = Console.ReadLine();
+Console.Write("Введите второе число ");
+string namberB = Console.ReadLine();
+Console.WriteLine(int.Parse(namberA) + int.Parse(namberB));
